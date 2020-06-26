7873a4966191e8421c684f8e4927d1eee0145cce
weresquirrel <vermokuss@gmail.com>
2020-06-26 13:32:56 +0100

    mocking the game, player always win yet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [isGameRunning, setIsGameRunning] = useState(false)
+  const [outcome, setOutcome] = useState('')
+  const [computersChoice, setComputersChoice] = useState('')
+
+  function runGame() {
+    setComputersChoice('rock')
+    setOutcome('win')
+    setIsGameRunning(true) 
+  }
+
+  function newGame() {
+    setComputersChoice('')
+    setIsGameRunning(false)
+  }
+
   return (
     <div className="App">
       <header>
-        <h1>Hello Rock!</h1>
+        <h1>o — x</h1>
       </header>
+      <main>
+        {/* Computer's choice */}
+        <div>
+          <p>Computer's choice:</p>
+          <p>
+            {computersChoice ? computersChoice : '???'}
+          </p>
+          
+        </div>
+
+        {/* Player's choice */}
+        <button onClick={runGame}>rock</button>
+        <button onClick={runGame}>paper</button>
+        <button onClick={runGame}>scissors</button>
+
+        {/* Outcome info + restart */}
+          {isGameRunning ? 
+            <div> 
+              <p>{outcome}</p>
+              <button onClick={newGame}>Play again</button>
+            </div> : 
+            null
+          }
+          
+      </main>
     </div>
   );
 }
