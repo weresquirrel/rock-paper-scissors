5d5e80aa87f071a0ad49b6fa7abb46995a9aaaab
weresquirrel <vermokuss@gmail.com>
2020-08-17 18:40:01 +0100

    firebase auth login added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
 
 import './App.css'
 import Game from './components/Game'
 
+const provider = new firebase.auth.GoogleAuthProvider()
+
+const Login = () => {
+  const onLogin = () => {
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .catch((error) => console.log(`Login error: ${error}`))
+  }
+
+  return <button onClick={onLogin}>Login</button>
+}
+
+const Logout = ({ user }) => {
+  return (
+    <>
+      <p>{user.displayName}</p>
+      <button
+        onClick={() => {
+          firebase.auth().signOut()
+        }}
+      >
+        Log out
+      </button>
+    </>
+  )
+}
+
 function App() {
+  const [user, setUser] = useState('no info')
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((u) => setUser(u))
+  }, [])
+
   return (
     <div className="App">
       <header>
         <h1>o — x</h1>
       </header>
       <main>
+        {user === 'no info' ? (
+          <p>loading...</p>
+        ) : user === null ? (
+          <Login />
+        ) : (
+          <Logout user={user} />
+        )}
         <Game />
       </main>
     </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import * as firebase from 'firebase/app'
+
+const config = {
+  apiKey: 'AIzaSyBV4CXMmv7KpmeyQrY6TJtNIN6pPllPYo0',
+  authDomain: 'rock-paper-scissors-70513.firebaseapp.com',
+  databaseURL: 'https://rock-paper-scissors-70513.firebaseio.com',
+  projectId: 'rock-paper-scissors-70513',
+  storageBucket: 'rock-paper-scissors-70513.appspot.com',
+  messagingSenderId: '426951715279',
+  appId: '1:426951715279:web:9e6601f153cf16968bcaba',
+}
+
+// Initialize Firebase
+firebase.initializeApp(config)
 
 ReactDOM.render(
   <React.StrictMode>
